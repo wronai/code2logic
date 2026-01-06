d5dbf8e0f4d18db2c31d97a6c9d1b77e3cc789cc
Tom Softreck <tom@sapletta.com>
2026-01-06 17:58:34 +0100

    refactoring

--- project.sh
+++ project.sh
@@ -7,3 +7,10 @@ poetry run code2logic code2logic -f toon -o project.c2l.toon --with-schema
 poetry run python reproduction_test_code.py
 poetry run python multilang_reproduction_tester.py --yaml project.c2l.yaml --hybrid project.c2l.hybrid.yaml --toon project.c2l.toon
 poetry run python universal_validator.py --yaml project.c2l.yaml --hybrid project.c2l.hybrid.yaml --toon project.c2l.toon --language python --verbose
+
+
+poetry run code2logic code2logic -f compact --function-logic -o project.func.logicml
+poetry run code2logic code2logic -f compact --function-logic -o project.func.json
+poetry run code2logic code2logic -f toon --function-logic -o project.func.yaml
+poetry run code2logic code2logic -f json --function-logic -o project.func.toon
+poetry run code2logic code2logic -f compact -q --function-logic project.func.logicml
