5516c96091c8d4eabc6b7c7816f6968c4794a918
Tom Softreck <tom@sapletta.com>
2026-01-08 22:31:30 +0100

    refactoring

--- code2logic/__init__.py
+++ code2logic/__init__.py
@@ -18,7 +18,7 @@ Example:
     >>> print(output)
 """
 
-__version__ = "1.0.19"
+__version__ = "1.0.20"
 __author__ = "Softreck"
 __email__ = "info@softreck.dev"
 __license__ = "MIT"
--- lolm/__init__.py
+++ lolm/__init__.py
@@ -64,7 +64,7 @@ from .manager import (
     list_available_providers,
 )
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 __all__ = [
     # Config
     'LLMConfig',
